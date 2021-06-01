cbe56025113fcbfb1a928be5953ab6f5c7ef4681
crpoudyal <crpoudyal@gmail.com>
2021-06-01 13:16:34 +0545

    datatypes in Dart

---
+++ datatype.dart
@@ -0,0 +1,23 @@
+void main() {
+  // # Built -  In - DataType
+  //Integer
+  int num = 12;
+  //double
+  double salary = 12.89;
+  //boolen
+  bool is_it = true;
+  //String
+  String my_name = "chudaraj";
+  //dynamic datatype
+  // dynamically set datatypes according to value
+  var a = "num";
+  var b = 12.3;
+  print(num);
+  print(salary);
+  print(my_name);
+  print(a);
+  print(b);
+
+  print(a.runtimeType);
+  print(is_it.runtimeType);
+}
