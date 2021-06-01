58d78d7151aa695babf74321520621bcf71e3fa0
crpoudyal <crpoudyal@gmail.com>
2021-06-01 10:34:41 +0545

    variables in Dart

---
+++ variable.dart
@@ -0,0 +1,22 @@
+void main() {
+  //Valid Variable naming
+  var name = "chudarj";
+  var f_name = 'cr';
+  var gpa = 3.25;
+  var is_rainning = false;
+  var city1 = "Biratnagar";
+  var $money = 30000;
+
+  print(name + " " + f_name);
+  print(gpa);
+  print(is_rainning);
+  print(city1);
+  print($money);
+
+  //Invalid Variable naming
+
+  // var 1person = "xyz";
+  // var #demo =1;
+  // var final =2;
+  //We cannot use reserved keyword as variable name
+}
